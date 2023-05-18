08ef10d232c5c45841f878725e33014296c57695
Wellington <wlima2santos@gmail.com>
2023-05-18 05:04:50 -0300

    criado server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,10 @@
-console.log('App index!!');
+import fastify from "fastify";
+
+const app = fastify();
+
+const port = 3333;
+app.listen({
+    port
+}).then(() => {
+    console.log(`🚀 Server running on port ${port}`);
+})
