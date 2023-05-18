084395f5646d94dd284418ff5a77297b6ce49ad2
Wellington <wlima2santos@gmail.com>
2023-05-18 06:06:11 -0300

    instalado e configurado prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,13 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
 const app = fastify();
+const prisma = new PrismaClient();
+
+app.get('/users', async()=> {
+    const users = await prisma.user.findMany();
+    return users;
+});
 
 const port = 3333;
 app.listen({
